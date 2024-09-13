dad08a10f1b4ed0237c5f77006f5c0b4dcf0e798
Yuri <gaoq4002@gmail.com>
2024-09-12 20:09:52 -0400

    uncommented all the test cases; forget to uncomment testaddAll ones

--- Java/src/test/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntListTest.java
+++ Java/src/test/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntListTest.java
@@ -46,34 +46,34 @@ public class DelegationSortedIntListTest {
     printList(list1);
     }
 
-    // /**
-    //  * Test for the addAll() method.
-    //  */
-    // @Test
-    // public void testAddAll() {
-    // // add 5 elements to our first list.
-    // list1.add(1);
-    // list1.add(3);
-    // list1.add(2);
-    // list1.add(4);
-    // list1.add(2);
+    /**
+     * Test for the addAll() method.
+     */
+    @Test
+    public void testAddAll() {
+    // add 5 elements to our first list.
+    list1.add(1);
+    list1.add(3);
+    list1.add(2);
+    list1.add(4);
+    list1.add(2);
 
-    // // check that the total number of elements added is 5.
-    // assertTrue(list1.getTotalAdded() == 5);
+    // check that the total number of elements added is 5.
+    assertTrue(list1.getTotalAdded() == 5);
 
-    // // add 2 elements to a second list.
-    // list2.add(3);
-    // list2.add(0);
+    // add 2 elements to a second list.
+    list2.add(3);
+    list2.add(0);
 
-    // // check that the total number of elements added is 2.
-    // assertTrue(list2.getTotalAdded() == 2);
+    // check that the total number of elements added is 2.
+    assertTrue(list2.getTotalAdded() == 2);
 
-    // // add the first list (5 elements) to our second list (2 elements).
-    // list2.addAll(list1);
+    // add the first list (5 elements) to our second list (2 elements).
+    list2.addAll(list1);
 
-    // // check that the total number of elements added to our second list is 7.
-    // assertTrue(list2.getTotalAdded() == 7);
-    // }
+    // check that the total number of elements added to our second list is 7.
+    assertTrue(list2.getTotalAdded() == 7);
+    }
 
     /**
      * A helper function that prints out the contents of an IntegerList.
